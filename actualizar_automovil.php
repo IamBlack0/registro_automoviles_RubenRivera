0352d50a0436e784b8fe26c63c150170acc2f6c0
black <blacknegocio3@gmail.com>
2024-09-03 15:48:29 -0500

    commit inicial

---
+++ actualizar_automovil.php
@@ -0,0 +1,5 @@
+<?php include('templates/header.php'); ?>
+
+hola
+
+<?php include('templates/footer.php'); ?>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <a href="registrar_automovil.php"class="button" >Registrar un nuevo automóvil</a>
         <a href="busqueda_automovil.php" class="button">Búsqueda</a>
         <a href="eliminar_automovil.php" class="button">Eliminación</a>
-        <a href="#" class="button">Actualización de registro</a>
+        <a href="actualizar_automovil.php" class="button">Actualización de registro</a>
     </div>
 
     <?php include('templates/footer.php'); ?>
